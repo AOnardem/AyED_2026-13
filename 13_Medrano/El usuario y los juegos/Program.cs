8ec751e5eeb1757ff8fb92c4002cb8411879691c
AOnardem <medranomamaniaracely@gmail.com>
2026-03-27 16:45:31 -0300

    Add files via upload

---
+++ 13_Medrano/El usuario y los juegos/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace El_usuario_y_los_juegos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            string juego;
+            string consola;
+            Console.WriteLine("Hola usuario");
+            Console.Write("¿Como te llamas?: ");
+            nombre = Console.ReadLine();
+            Console.Write("¿Cual es tu juego favorito?: ");
+            juego = Console.ReadLine();
+            Console.Write("¿en que consola se juega?: ");
+            consola = Console.ReadLine();
+            Console.WriteLine("Hola " + nombre + ". Tu juego favorito es " + juego + " y se juega en " + consola);
+        }
+    }
+}
