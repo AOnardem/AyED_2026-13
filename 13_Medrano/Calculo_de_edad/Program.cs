9b7603c7a6732e87630a74578635c0ac5037d243
AOnardem <medranomamaniaracely@gmail.com>
2026-04-01 14:27:10 -0300

    Add files via upload

---
+++ 13_Medrano/Calculo_de_edad/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculo_de_edad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("¿Que año naciste?: ");
+            int año = Convert.ToInt32(Console.ReadLine());
+            int edad = 2026 - año;
+            Console.WriteLine("Si naciste en el año " + año + " tu edad al comienzo del año es de " + edad + " año ");
+        }
+    }
+}
