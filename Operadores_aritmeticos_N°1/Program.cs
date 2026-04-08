4f6a8c1afd17b5b8a3c18192b5fcfbc366b67616
AOnardem <medranomamaniaracely@gmail.com>
2026-04-08 14:24:12 -0300

    Add files via upload

---
+++ Operadores_aritmeticos_N°1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operadores_aritmeticos_N_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Escribe un valor: ");
+            int valor_1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Escribe un valor: ");
+            int valor_2 = Convert.ToInt32(Console.ReadLine());
+            int suma = valor_1 + valor_2;
+            int resta = valor_1 - valor_2;
+            int multi = valor_1 * valor_2;
+            int divicion = valor_1 / valor_2;
+            Console.WriteLine("El resultado de la suma de los dos valores es: " + suma + ". El resultado de la resta de los dos valores es: " + resta + ". El resultado de la multiplicación de los dos valores es: " + multi + ". El resultado de la divicion de los dos valores es: " + divicion);
+        }
+    }
+}
