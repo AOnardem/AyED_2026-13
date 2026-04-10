cbcbb0902a50085a8a704c1fc844d9b7e3449bc9
AOnardem <medranomamaniaracely@gmail.com>
2026-04-10 15:35:11 -0300

    Add files via upload

---
+++ Ejercitacion_IF_ELSE/Nivel_de_midiclorianos/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nivel_de_midiclorianos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese la cantidad de midiclorianos: ");
+            int cantidad = Convert.ToInt32(Console.ReadLine());
+            if (cantidad > 15000)
+            {
+                Console.WriteLine("Potencial Maestro Jedi detectado. Avisar al Consejo");
+            }
+            else
+            {
+                if (cantidad >= 5000)
+                {
+                    Console.WriteLine("Aceptable para entrenamiento Padawan");
+                }
+                else
+                {
+                    Console.WriteLine("No posee sensibilidad a la Fuerza");
+                }
+
+            }
+
+        }
+    }
+}
