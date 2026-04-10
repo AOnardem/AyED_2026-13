b6eb86c4e4580ad4e92e524abe27ccf5e5f8def9
AOnardem <medranomamaniaracely@gmail.com>
2026-04-10 14:41:53 -0300

    Add files via upload

---
+++ Ejercitacion_IF_ELSE/Control_de_Peso/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Control_de_Peso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Escribe cual es el peso actual: ");
+            int peso = Convert.ToInt32(Console.ReadLine());
+            if (peso <= 5000)
+            {
+                Console.WriteLine("Despegue autorizado. Buen viaje, Mando");
+            }
+            else
+            {
+                int peso_quitar = peso - 5000;
+                Console.WriteLine("Alerta: Exceso de peso. Debes descargar " + peso_quitar + " kg para despegar");
+            }
+        }
+    }
+}
