fb89ebafaa4232905bb16673281bfce2ccc1b338
AOnardem <medranomamaniaracely@gmail.com>
2026-04-10 16:13:54 -0300

    Add files via upload

---
+++ Ejercitacion_IF_ELSE/Calculo_de_salto_hiperespacio/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculo_de_salto_hiperespacio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Escriba la distacia al destino (en años luz): ");
+            int distancia = Convert.ToInt32(Console.ReadLine());//10
+            Console.Write("Escriba la cantidad de conbustible: ");//120
+            int conbustible = Convert.ToInt32(Console.ReadLine());
+            int can_l_v = distancia * 12;
+
+            if (conbustible >= can_l_v) 
+            {
+                Console.WriteLine("Cálculos precisos. Saltando al hiperespacio");
+            }
+
+            else
+            {
+                int can_a_bajar = can_l_v - conbustible;
+                Console.WriteLine("Peligro: Combustible insuficiente. Faltan " + can_a_bajar + " litros");
+            }
+
+        }
+    }
+}
