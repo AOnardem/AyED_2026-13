6ed8b6e43f659b18bbb7717902a3c9d5f7e6f0b6
AOnardem <medranomamaniaracely@gmail.com>
2026-04-15 15:28:27 -0300

    Add files via upload

---
+++ Condicionales_Primera_parte/Par_o_Impar/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Par_o_Impar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Escriba un numero para ver si es par o impar: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            int resultado = numero % 2;
+            if (resultado == 0)
+            {
+                Console.WriteLine("Su numero es par");
+            }
+            else
+            {
+                Console.WriteLine("Su numero es impar");
+            }
+        }
+    }
+}
