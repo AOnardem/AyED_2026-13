91767bd280020800050842118c409c3057d9b6de
AOnardem <medranomamaniaracely@gmail.com>
2026-04-15 16:18:04 -0300

    Add files via upload

---
+++ Condicionales_Primera_parte/Usuario_Contraseña/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Usuario_Contraseña
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Escriba su usuario: ");
+            String usuario = Console.ReadLine();
+            Console.Write("Escriba su contraceña: ");
+            String contraseña = Console.ReadLine();
+            if (usuario == "admin")
+            {
+                if(contraseña == "1234")
+                {
+                    Console.WriteLine("Acceso concedido");
+                }
+                else
+                {
+                    Console.WriteLine("Error de credenciales"); 
+                }
+  
+            }
+            
+        }
+    }
+}
