f6b37e7d05e9cb68d312d8f445bb2f30e6ff0b19
AOnardem <medranomamaniaracely@gmail.com>
2026-04-15 16:29:14 -0300

    Add files via upload

---
+++ Condicionales_Primera_parte/Clima_de_la_Montaña/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clima_de_la_Montaña
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Escriba cual es la temperatura actual de la montaña: ");
+            int temperatura = Convert.ToInt32(Console.ReadLine());
+            if (temperatura <= 0)
+            {
+                Console.WriteLine("Peligro de congelamiento");
+            }
+            else
+            {
+                if (temperatura <= 15)
+                {
+                    Console.WriteLine("Mucho frío");
+                }
+                else
+                {
+                    if (temperatura > 15)
+                    {
+                        Console.WriteLine("Clima agradable");
+                    }
+                }
+            }
+        }
+    }
+}
