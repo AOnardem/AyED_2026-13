c56ca884df222f988445a4c1e9e2eb2f74b8a9cd
AOnardem <medranomamaniaracely@gmail.com>
2026-04-15 16:46:12 -0300

    Add files via upload

---
+++ Condicionales_Primera_parte/multiplos_de_5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multiplos_de_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Escribe un numero: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            int resultado = numero % 5;
+            if (resultado == 0)
+            {
+                Console.WriteLine("El numero que puso es multiplo de 5");
+            }
+            else
+            {
+                Console.WriteLine("El numero que puso no es multiplo de 5");
+            }
+        }
+    }
+}
