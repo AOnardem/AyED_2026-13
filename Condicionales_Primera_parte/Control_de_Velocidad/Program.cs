16a2c680b123a1cd96d44558cce7ac31d394b637
AOnardem <medranomamaniaracely@gmail.com>
2026-04-15 14:39:21 -0300

    Add files via upload

---
+++ Condicionales_Primera_parte/Control_de_Velocidad/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Control_de_Velocidad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Escriba la velocidad de un vehiculo: ");
+            int velocidad = Convert.ToInt32(Console.ReadLine());
+            if (velocidad > 120)
+            {
+                Console.WriteLine("Multa en camino: Exceso de velocidad");
+            }
+            else
+            {
+                Console.WriteLine("Conducción segura");
+            }
+        
+        }
+    }
+}
