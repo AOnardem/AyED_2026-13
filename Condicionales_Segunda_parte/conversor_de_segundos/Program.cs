a47a300884a5ef934e907ef4ff759cefbcf1f282
AOnardem <medranomamaniaracely@gmail.com>
2026-04-24 16:11:30 -0300

    Add files via upload

---
+++ Condicionales_Segunda_parte/conversor_de_segundos/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conversor_de_segundos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Escriba una cantidad de segundos: ");
+            int seg = Convert.ToInt32(Console.ReadLine());
+            int min = seg / 60;
+            int segundos = seg % 60;
+            if (seg > 60)
+            {
+                Console.WriteLine("Son " + min + " minutos con " + segundos + " segundos");
+            }
+            else
+            {
+                Console.WriteLine("Son " + seg + " segundos");
+            }
+        }
+    }
+}
