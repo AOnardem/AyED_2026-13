19e8d199c55863dc001443894a1d9720624c08f1
AOnardem <medranomamaniaracely@gmail.com>
2026-04-24 15:26:18 -0300

    Add files via upload

---
+++ Condicionales_Segunda_parte/Calculadora_de_IMC/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora_de_IMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese su peso: ");
+            int peso = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ingrese su altura: ");
+            int altura = Convert.ToInt32(Console.ReadLine());
+            int IMC = peso / (altura * 2);
+            if (IMC > 25)
+            {
+                Console.WriteLine("Sobrepeso");
+            }
+            else
+            {
+                Console.WriteLine("Rango Normal");
+            }
+
+        }
+    }
+}
