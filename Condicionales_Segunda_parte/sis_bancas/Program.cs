e6857706ac077e5820a32023b61005159573c8c4
AOnardem <medranomamaniaracely@gmail.com>
2026-04-24 16:19:17 -0300

    Add files via upload

---
+++ Condicionales_Segunda_parte/sis_bancas/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sis_bancas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Escriba su promedio: ");
+            int pro = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Escriba su distancia: ");
+            int dis = Convert.ToInt32(Console.ReadLine());
+            if (pro > 8 || dis > 50)
+            {
+                Console.WriteLine("Beca conceguida");
+            }
+        }
+    }
+}
