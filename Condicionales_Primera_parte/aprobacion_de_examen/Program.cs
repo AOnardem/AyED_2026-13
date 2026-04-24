c07e60c31a22f12082edb1b4c1cf68e10245ee22
AOnardem <medranomamaniaracely@gmail.com>
2026-04-24 14:39:50 -0300

    Add files via upload

---
+++ Condicionales_Primera_parte/aprobacion_de_examen/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aprobacion_de_examen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Escriba la nota del examen: ");
+            double nota = Convert.ToDouble(Console.ReadLine());
+            if (nota >= 7)
+            {
+                Console.WriteLine("Promocionado");
+            }
+            else
+            {
+                if(nota <= 6 )
+                {
+                    Console.WriteLine("A finales");
+                }
+                else
+                {
+                    if(nota <= 4)
+                    {
+                        Console.WriteLine("Recuperatorio");
+                    }
+                }
+            }
+        }
+    }
+}
