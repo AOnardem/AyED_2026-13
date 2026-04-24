6729e2a76e635d375bcba8c73a21418e86234dda
AOnardem <medranomamaniaracely@gmail.com>
2026-04-24 16:43:13 -0300

    Add files via upload

---
+++ Condicionales_Segunda_parte/comparador_de_3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace comparador_de_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Escribe N°1: ");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Escribe N°2: ");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Escribe N°3: ");
+            int n3 = Convert.ToInt32(Console.ReadLine());
+            if (n1 > n2 && n2 > n3)
+            {
+                Console.WriteLine( n1 + " es mayor");
+            }
+            else
+            {
+                if (n2 > n1 && n1 > n3)
+                {
+                    Console.WriteLine( n2 + " es mayor");
+                }
+                else
+                {
+                    Console.WriteLine( n3 + " es mayor");
+                }
+            }
+
+        }
+    }
+}
