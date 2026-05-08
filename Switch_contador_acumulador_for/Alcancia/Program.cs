0094d1f0c80074ab762ecfd05c8a25417f264d93
AOnardem <medranomamaniaracely@gmail.com>
2026-05-08 14:32:45 -0300

    Add files via upload

---
+++ Switch_contador_acumulador_for/Alcancia/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alcancia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ahorroAcu = 0;
+            int ahorro = 0;
+            for (int i=1 ; i<=4 ; i++)
+            {
+                Console.Write("Ingrese el ahorro de la semana N° " + i + ":");
+                ahorro = int.Parse(Console.ReadLine());
+                ahorroAcu = ahorroAcu + ahorro;
+            }
+            Console.WriteLine("El dinero acumulado en el mes es de " + ahorroAcu);
+        }
+    }
+}
