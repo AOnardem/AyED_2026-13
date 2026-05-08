d804a2321cdda8aa3ec2eb4c19a56338f7ac980b
AOnardem <medranomamaniaracely@gmail.com>
2026-05-08 15:55:05 -0300

    Add files via upload

---
+++ Switch_contador_acumulador_for/Registro_temperatura/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Registro_temperatura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int temAcu = 0;
+            int tem = 0;
+            int contador = 0;
+            for (int i=1 ; i<=7 ; i++)
+            {
+                Console.Write("Ingrese la temperatura del dia N° " + i + ": ");
+                tem = int.Parse(Console.ReadLine());
+                temAcu = temAcu + tem;
+                if(tem < 0)
+                {
+                    contador++;
+                }
+            }
+            Console.WriteLine("Fuero " + contador + " dias de la semana que la temperatura fue menor a 0");
+        }
+    }
+}
