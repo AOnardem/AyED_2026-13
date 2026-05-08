8dd71f4b875526005cb04ea6a241a53b97f4555e
AOnardem <medranomamaniaracely@gmail.com>
2026-05-08 14:16:54 -0300

    Add files via upload

---
+++ Switch_contador_acumulador_for/Clacificador_clima/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clacificador_clima
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Estas son las opciones a tomar:");
+            Console.WriteLine("1. primavera");
+            Console.WriteLine("2. verano");
+            Console.WriteLine("3. otoño");
+            Console.WriteLine("4. invierno");
+            Console.Write("Coloque la opcion deciada: ");
+            int opcion = int.Parse(Console.ReadLine());
+            switch (opcion)
+            {
+                case 1:
+                    Console.WriteLine("La opcion elegida es primavera");
+                    break;
+                case 2:
+                    Console.WriteLine("La opcion elegida es verano");
+                    break;
+                case 3:
+                    Console.WriteLine("La opcion elegida es otoño");
+                    break;
+                case 4:
+                    Console.WriteLine("La opcion elegida es invierno");
+                    break;
+                default:
+                    Console.WriteLine("La opcion elegida no es valida");
+                    break;
+            }
+
+
+
+        }
+    }
+}
