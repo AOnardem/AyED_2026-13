4387a1e609051d8d1e5659231858129778940c3d
AOnardem <medranomamaniaracely@gmail.com>
2026-05-20 14:28:58 -0300

    Add files via upload

---
+++ While_Do_while/La cuenta regreciva del despegue/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace La_cuenta_regreciva_del_despegue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("ingrese un numero positivo: ");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine("Cuenta regresiva: ");
+            while (num >= 0)
+            {
+                Console.WriteLine( num);
+                num--;
+            }
+
+        }
+    }
+}
