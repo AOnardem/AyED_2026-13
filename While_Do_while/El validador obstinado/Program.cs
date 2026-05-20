974f48b3950133b461424561102a389180a50c97
AOnardem <medranomamaniaracely@gmail.com>
2026-05-20 15:49:49 -0300

    Add files via upload

---
+++ While_Do_while/El validador obstinado/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace El_validador_obstinado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("La nota es: ");
+            int num = int.Parse(Console.ReadLine());
+            if ( num > 10)
+            {
+                do
+                {
+                    Console.Write("ERROR intentelo otra vez: ");
+                    num = int.Parse(Console.ReadLine());
+
+                } while (num < 1 || num > 10);
+                Console.WriteLine("La nota es valida " + num);
+            }
+           
+        }
+    }
+}
