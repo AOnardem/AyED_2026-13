a66274b6ec7aa264e48882ef3495f380f9fcc15b
AOnardem <medranomamaniaracely@gmail.com>
2026-05-22 16:45:27 -0300

    Add files via upload

---
+++ While_Do_while/Tabla de multiplicar personalizada/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabla_de_multiplicar_personalizada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cont = 1;
+            Console.Write("Ingrese el numero: ");
+            int num = int.Parse(Console.ReadLine()); 
+            while (cont <= 12 )
+            {
+                Console.WriteLine(num + " x " +  cont + (" = ") + num * cont);
+                cont++; 
+            } 
+
+        }
+    }
+}
