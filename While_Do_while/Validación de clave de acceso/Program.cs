7aa1614b1acefd878565d3f7091ec3cf2d2bbfd0
AOnardem <medranomamaniaracely@gmail.com>
2026-05-22 16:10:09 -0300

    Add files via upload

---
+++ While_Do_while/Validación de clave de acceso/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Validación_de_clave_de_acceso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese la clave de acceso: ");
+           string clave = (Console.ReadLine());
+            if (clave != "BL")
+            {
+                do
+                {
+                    Console.Write("ERROR intentelo otra vez: ");
+                    clave = (Console.ReadLine());
+                } while (clave != "BL");
+            }
+            Console.WriteLine("Acceso Permitido :)" );
+        }
+    }
+}
