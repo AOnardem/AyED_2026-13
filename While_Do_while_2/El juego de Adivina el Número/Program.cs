ff09683767890808b427d7e94ce637ad34f5d5c1
AOnardem <medranomamaniaracely@gmail.com>
2026-06-10 14:53:49 -0300

    Add files via upload

---
+++ While_Do_while_2/El juego de Adivina el Número/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace El_juego_de_Adivina_el_Número
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numsec = 42;
+            int intentos = 0;
+            int num;
+
+            do
+            {
+                Console.Write("Adivine el número: ");
+                num = int.Parse(Console.ReadLine());
+
+                intentos++;
+
+                if (num < numsec)
+                {
+                    Console.WriteLine("El número es mayor");
+                }
+                else if (num > numsec)
+                {
+                    Console.WriteLine("El número es menor");
+                }
+
+            } while (num != numsec && intentos < 5);
+
+            if (num == numsec)
+            {
+                Console.WriteLine("Ganaste");
+            }
+            else
+            {
+                Console.WriteLine("Perdiste. El número era " + numsec);
+            }
+
+            Console.WriteLine("Intentos utilizados: " + intentos);
+        
+
+        }
+    }
+}
