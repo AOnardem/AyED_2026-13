6abee8ca3599fb93d690aa65f63b9770ac26853d
AOnardem <medranomamaniaracely@gmail.com>
2026-06-10 15:46:10 -0300

    Add files via upload

---
+++ While_Do_while_2/Sis de login con intentos bloqueados/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sis_de_login_con_intentos_bloqueados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string usuco = "admin";
+            string conco = "1234";
+
+            int intentos = 0;
+
+            while (intentos < 3)
+            {
+                Console.Write("Usuario: ");
+                string usuario = Console.ReadLine();
+
+                Console.Write("Contraseña: ");
+                string contraseña = Console.ReadLine();
+
+                if (usuario == usuco && contraseña == conco)
+                {
+                    Console.WriteLine("Bienvenido al sistema.");
+                }
+
+                intentos++;
+            }
+
+            if (intentos == 3)
+            {
+                Console.WriteLine("Cuenta bloqueada por seguridad.");
+            }
+        }
+    }
+}
