3ce4320f874debf4e187d38441421e8a3d1c6feb
AOnardem <medranomamaniaracely@gmail.com>
2026-06-12 14:43:55 -0300

    Add files via upload

---
+++ While_Do_while_2/Contador números positivo negativo cero/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contador_números_positivo_negativo_cero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pos = 0;
+            int neg = 0;
+            int ceros = 0;
+
+            string entrada;
+
+            while (true)
+            {
+                Console.Write("Ingrese un número: ");
+                entrada = Console.ReadLine();
+
+                if (entrada == "X")
+                {
+                    break;
+                }
+
+                int numero = int.Parse(entrada);
+
+                if (numero > 0)
+                {
+                    pos++;
+                }
+                else if (numero < 0)
+                {
+                    neg++;
+                }
+                else
+                {
+                    ceros++;
+                }
+            }
+
+            Console.WriteLine("Positivos: " + pos);
+            Console.WriteLine("Negativos: " + neg);
+            Console.WriteLine("Ceros: " + ceros);
+        }
+    }
+}
