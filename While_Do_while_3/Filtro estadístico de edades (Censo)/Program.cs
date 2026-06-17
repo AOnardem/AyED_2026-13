d15e4f921c7315167e132de07f479cd589e37bad
AOnardem <medranomamaniaracely@gmail.com>
2026-06-17 15:26:09 -0300

    Add files via upload

---
+++ While_Do_while_3/Filtro estadístico de edades (Censo)/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filtro_estadístico_de_edades__Censo_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad;
+            int mayores = 0;
+            int menores = 0;
+            int cantidad = 0;
+            int suma = 0;
+
+            Console.Write("Ingrese una edad: ");
+            edad = Convert.ToInt32(Console.ReadLine());
+
+            while (edad >= 0)
+            {
+                suma += edad;
+                cantidad++;
+
+                if (edad >= 18)
+                    mayores++;
+                else
+                    menores++;
+
+                Console.Write("Ingrese una edad: ");
+                edad = Convert.ToInt32(Console.ReadLine());
+            }
+
+            double promedio = (double)suma / cantidad;
+
+            Console.WriteLine("Promedio de edad: " + promedio);
+            Console.WriteLine("Mayores de edad: " + mayores);
+            Console.WriteLine("Menores de edad: " + menores);
+
+        }
+    }
+}
