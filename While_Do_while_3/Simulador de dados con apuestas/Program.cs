a0a8e42691515e716290ce096d77264c6471f7c7
AOnardem <medranomamaniaracely@gmail.com>
2026-06-17 16:27:45 -0300

    Add files via upload

---
+++ While_Do_while_3/Simulador de dados con apuestas/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulador_de_dados_con_apuestas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int creditos = 100;
+            int apuesta;
+            int dad1;
+            int dad2;
+            int suma;
+
+            do
+            {
+                Console.WriteLine("Créditos disponibles: " + creditos);
+
+                Console.Write("Ingrese su apuesta: ");
+                apuesta = int.Parse(Console.ReadLine());
+                if (apuesta == 0)
+                {
+                    break;
+                }                  
+
+                if (apuesta > creditos || apuesta < 0)
+                {
+                    Console.WriteLine("Apuesta inválida.");
+                }
+
+                Console.Write("Ingrese el valor del dado 1: ");
+                dad1 = int.Parse(Console.ReadLine());
+
+                Console.Write("Ingrese el valor del dado 2: ");
+                dad2 = int.Parse(Console.ReadLine());
+
+                suma = dad1 + dad2;
+
+                if (suma == 7 || suma == 11)
+                {
+                    creditos += apuesta * 2;
+                    Console.WriteLine("Ganaste");
+                }
+                else
+                {
+                    creditos -= apuesta;
+                    Console.WriteLine("Perdiste");
+                }
+
+            } while (creditos != 0 );
+
+            Console.WriteLine("Saldo final: " + creditos);
+
+        }
+    }
+}
