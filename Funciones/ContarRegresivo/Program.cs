513508a36e46dbcf732392e622295dcb455806d7
AOnardem <medranomamaniaracely@gmail.com>
2026-08-28 14:40:47 -0300

    Add files via upload

---
+++ Funciones/ContarRegresivo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContarRegresivo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingresa un numero: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            cuenta_regresiva(num);
+        }
+
+        static void cuenta_regresiva(int num)
+        {
+            while(num >= 1)
+            {
+                Console.WriteLine(num);
+                num--;
+            }
+        }
+    }
+}
