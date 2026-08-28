12e95af27fc5d14dff2b2a67d2489c6b33a5b540
AOnardem <medranomamaniaracely@gmail.com>
2026-08-28 15:25:07 -0300

    Add files via upload

---
+++ Funciones/ContarHasta/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContarHasta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingresa un numero: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            contador_hasta(num);
+        }
+        static void contador_hasta (int num)
+        {
+            int cont = 1;
+            while (cont <= num)
+            {
+                Console.WriteLine(cont);
+                cont++; 
+            }
+        }
+    }
+}
