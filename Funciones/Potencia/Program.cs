fbc4e128f582cab7129333d2dca18cfde07301a4
AOnardem <medranomamaniaracely@gmail.com>
2026-08-28 16:49:55 -0300

    Add files via upload

---
+++ Funciones/Potencia/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Potencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un numero como base: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ingrese un numero como exponente: ");
+            int po = Convert.ToInt32(Console.ReadLine());
+            int resul = potencia(num, po);
+            Console.WriteLine("El resultado es: " + resul);
+        }
+
+        static int potencia (int num, int po)
+        {
+            int resul = 1;
+            for (int i = 0; i < po; i++)
+            {
+                resul = resul * num;
+            }
+            return resul;
+        }
+    }
+}
